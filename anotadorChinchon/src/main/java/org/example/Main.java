c33649fd562656f49a6b338cfb19ca78fa842fd8
Kevn1Dev <kevinnicolas302@gmail.com>
2025-01-25 11:45:04 -0300

    pomm

--- anotadorChinchon/src/main/java/org/example/Main.java
+++ anotadorChinchon/src/main/java/org/example/Main.java
@@ -2,11 +2,9 @@ package org.example;
 
 import data.DatabaseConfig;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+
         // Probar la conexión
         if (DatabaseConfig.getConnection() != null) {
             System.out.println("Conexión verificada y funcionando correctamente.");
